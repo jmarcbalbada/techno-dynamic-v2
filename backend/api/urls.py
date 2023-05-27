40febf4e571730f958cd550377abb97dd022e446
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-28 02:39:06 +0800

    Remove Code Smell

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,6 @@ from .views import LessonsView, LessonDetailView
 
 urlpatterns = [
     path('lessons/', LessonsView.as_view()),
-    path('lessons/<int:lesson_id>/', LessonDetailView.as_view()),
+    path('lessons/<int:lesson_id>', LessonDetailView.as_view()),
 
 ]
