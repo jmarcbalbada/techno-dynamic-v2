9755f49d0a41421018e590dc4cbcbe15720fd662
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-28 01:46:01 +0800

    add ID serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Lesson
 
 class LessonSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+
     class Meta:
         model = Lesson
         fields = ['id', 'title', 'content']
