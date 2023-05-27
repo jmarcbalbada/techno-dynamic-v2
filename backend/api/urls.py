2eccac7e2ea9612dae94ab864622d9070ddfacd3
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-28 02:04:59 +0800

    Implement deleteLesson

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import getLessons, createLesson, getLessonById, updateLesson
+from .views import getLessons, createLesson, getLessonById, updateLesson, deleteLesson
 
 urlpatterns = [
     path('lessons/', getLessons),
     path('lessons/create/', createLesson),
     path('lessons/<int:lesson_id>', getLessonById),
-    path('lessons/update/<int:lesson_id>', updateLesson)
+    path('lessons/update/<int:lesson_id>', updateLesson),
+    path('lessons/delete/<int:lesson_id>', deleteLesson)
 ]
