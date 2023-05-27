f91e542c9d0b4cd974805ff58dfb233c58a84516
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-28 02:36:23 +0800

    Improve CRUD for Lessons

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,50 +1,46 @@
 from django.http import JsonResponse
 from .models import Lesson
 from .serializers import LessonSerializer
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
-@api_view(['GET'])
-def getLessons(request):
-    lessons = Lesson.objects.all()
-    serializer = LessonSerializer(lessons, many=True)
-    return JsonResponse({"lessons:": serializer.data}, safe=False)
+class LessonsView(APIView):
+    @staticmethod
+    def get(self, request):
+        lessons = Lesson.objects.all()
+        serializer = LessonSerializer(lessons, many=True)
+        return JsonResponse({"lessons:": serializer.data}, safe=False)
 
-@api_view(['GET'])
-def getLessonById(request, lesson_id):
-    lesson = checkLessonIfExist(lesson_id)
-    serializer = LessonSerializer(lesson)
-    return Response(serializer.data)
+    @staticmethod
+    def post(self, request):
+        serializer = LessonSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST'])
-def createLesson(request):
-    serializer = LessonSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class LessonDetailView(APIView):
+    @staticmethod
+    def get(self, request, lesson_id):
+        lesson = get_object_or_404(Lesson, pk=lesson_id)
+        serializer = LessonSerializer(lesson)
+        return Response(serializer.data)
 
-@api_view(['PUT'])
-def updateLesson(request, lesson_id):
-    lesson = checkLessonIfExist(lesson_id)
-    serializer = LessonSerializer(lesson, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @staticmethod
+    def put(self, request, lesson_id):
+        lesson = get_object_or_404(Lesson, pk=lesson_id)
+        serializer = LessonSerializer(lesson, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['DELETE'])
-def deleteLesson(request, lesson_id):
-    lesson = checkLessonIfExist(lesson_id)
-    lesson.delete()
-    return Response("Lesson deleted successfully", status=status.HTTP_204_NO_CONTENT)
-
-def checkLessonIfExist(lesson_id):
-    try:
-        lesson = Lesson.objects.get(pk=lesson_id)
-    except Lesson.DoesNotExist:
-        return Response("Lesson not found", status=status.HTTP_404_NOT_FOUND)
-    return lesson
+    @staticmethod
+    def delete(self, request, lesson_id):
+        lesson = get_object_or_404(Lesson, pk=lesson_id)
+        lesson.delete()
+        return Response("Lesson deleted successfully", status=status.HTTP_204_NO_CONTENT)
