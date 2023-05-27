7c0947b7c8639e08efb5297697b352c5776665a8
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-28 01:24:17 +0800

    Add lesson_view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Lesson
+from .serializers import LessonSerializer
 
 # Create your views here.
+
+def lesson_view(request):
+    lessons = Lesson.objects.all()
+    serializer = LessonSerializer(lessons, many=True)
+    return JsonResponse(serializer.data, safe=False)
