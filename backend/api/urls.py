5951da5b2d51241ccf7e69479355a6cf9d00e0a4
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-28 01:55:15 +0800

    Implement getLessonById

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import getLessons, createLesson
+from .views import getLessons, createLesson, getLessonById
 
 urlpatterns = [
     path('lessons/', getLessons),
     path('lessons/create/', createLesson),
+    path('lessons/<int:id>', getLessonById)
 ]
