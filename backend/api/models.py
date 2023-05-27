d998ed5fab40cc7f03a75d948d7c3d5606c9d5e9
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-28 00:44:26 +0800

    Create Lesson Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Lesson(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50, default="", unique=True, null=False)
+    content = models.CharField(max_length=5000, default="", null=False)
