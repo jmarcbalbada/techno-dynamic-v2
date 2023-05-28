d1e051a84932ebc44987573883b1b4b2ea033372
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-29 03:33:35 +0800

    Modify Serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
-from .models import Lesson
-from django import forms
+from .models import Lesson, LessonContents
+
+class LessonContentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LessonContents
+        fields = ['id', 'contents']
+
 
 class LessonSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
+    lesson_contents = LessonContentsSerializer(many=True, read_only=True)
 
     class Meta:
         model = Lesson
-        fields = ['id', 'title', 'content', 'file']
+        fields = ['id', 'title', 'lesson_contents', 'url', 'file']
