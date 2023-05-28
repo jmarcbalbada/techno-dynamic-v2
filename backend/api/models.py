d7d590e2a656125085cba7f3972c0ae9879c362b
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-29 03:33:25 +0800

    Create LessonContents Table

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,9 +4,18 @@ from django.db import models
 
 class Lesson(models.Model):
     title = models.CharField(max_length=50, default="", unique=True, null=False)
-    content = models.CharField(max_length=5000, default="", null=False)
     url = models.URLField(max_length=255, null=True, blank=True)
     file = models.FileField(upload_to='media/', null=True, blank=True)
 
+    def get_contents(self):
+        return LessonContents.objects.filter(lesson=self)
+
     def __str__(self):
-        return self.title + ' ' + self.content
+        return self.title
+
+class LessonContents(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    contents = models.TextField(default="", null=False)
+
+    def __str__(self):
+        return self.contents
