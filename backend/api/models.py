4af134b231a11799845d882fbb62fed4984d425d
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-29 02:00:30 +0800

    Implement add file

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
 class Lesson(models.Model):
     title = models.CharField(max_length=50, default="", unique=True, null=False)
     content = models.CharField(max_length=5000, default="", null=False)
-    url = models.URLField(max_length=255, null=True)
+    url = models.URLField(max_length=255, null=True, blank=True)
+    file = models.FileField(upload_to='media/', null=True, blank=True)
 
     def __str__(self):
         return self.title + ' ' + self.content
