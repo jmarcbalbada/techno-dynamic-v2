a96d22df2aae4998b27981b287e89177f941100f
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-05-28 16:00:33 +0800

    Create Base Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
+import { Routes, Route } from 'react-router'
+import { Dashboard } from './components/Dashboard/Dashboard'
+import { LessonPage } from './components/LessonPage/LessonPage'
+import { NotFound } from './components/NotFound/NotFound'
 import './App.css'
 
 const App = () => {
 
   return (
-    <span class="material-icons">
-      search home
-    </span>
+    <>
+      <Routes>
+
+        <Route path='/' element={<Dashboard />}>
+        </Route>
+
+        <Route path='lesson' element={<LessonPage />}>
+        </Route>
+
+        <Route path='*' element={<NotFound />}></Route>
+
+      </Routes>
+    </>
   )
 }
 
