6c24acacac7b1321dfb38da743b7bb172dd930b6
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-29 02:00:30 +0800

    Implement add file

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Lesson
+from django import forms
 
 class LessonSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
 
     class Meta:
         model = Lesson
-        fields = ['id', 'title', 'content']
+        fields = ['id', 'title', 'content', 'file']
