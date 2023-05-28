9fae154112b5b9ab4534d43ae52b369a5fc50857
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-29 03:33:13 +0800

    Register LessonContents

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Lesson
+from .models import Lesson, LessonContents
 
 # Register your models here.
 admin.site.register(Lesson)
+admin.site.register(LessonContents)
