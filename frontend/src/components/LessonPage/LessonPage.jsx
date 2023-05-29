b97fcd378c34f11859c49fcea50025023e83c63d
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-05-29 22:25:23 +0800

    Update Lesson Pages and Editor

--- frontend/src/components/LessonPage/LessonPage.jsx
+++ frontend/src/components/LessonPage/LessonPage.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Container } from '@mui/material'
+import { BottomControls } from './BottomControls'
+import './LessonPage.css'
 
 export const LessonPage = () => {
   return (
-    <div>LessonPage</div>
+    <div>
+      <Container maxWidth='md' className='mb-6rem'>
+        <div className='lesson-page-container'>
+          <div className='lesson-page-title'>
+            <h1>Lesson 1</h1>
+            <div className="vertical-line"></div>
+            <h2>asd</h2>
+          </div>
+          <div className='lesson-page-content'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores autem temporibus alias, fuga, voluptate, repudiandae error praesentium in eveniet repellendus maiores eos perspiciatis aspernatur veritatis nobis delectus. Quis, sapiente inventore. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, corrupti perspiciatis quidem veritatis reprehenderit amet, harum rem optio nesciunt ducimus modi obcaecati facilis reiciendis. Eos velit dolorum ipsa sequi nesciunt!</p>
+          </div>
+        </div>
+      </Container>
+      <BottomControls />
+    </div>
   )
 }
