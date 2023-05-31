2577d5ce400c56c6c5c099bde7c047e09157d564
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-05-31 17:47:38 +0800

    Cleanup Code

--- frontend/src/components/EndLessonPage/EndLessonPage.jsx
+++ frontend/src/components/EndLessonPage/EndLessonPage.jsx
@@ -1,13 +1,12 @@
-import { Container, CardMedia } from '@mui/material'
+import { Container, CardMedia } from '@mui/material';
 import { EndBottomControls } from './EndBottomControls';
-import './EndLessonPage.css'
+import './EndLessonPage.css';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getLesson } from '../../apis/Lessons';
 
 export const EndLessonPage = () => {
-
-	const {lessonid} = useParams();
+	const { lessonid } = useParams();
 	const [lesson, setLesson] = useState({});
 
 	useEffect(() => {
@@ -18,30 +17,30 @@ export const EndLessonPage = () => {
 		})();
 	}, []);
 
-  return (
-    <>
-				<div>
-					<Container maxWidth="md" className="mb-6rem">
-						<div className="end-lesson-page-container">
-							<div className="end-lesson-page-title">
-								<h1>Lesson {lessonid}</h1>
-								<div className="vertical-line"></div>
-								<h2>{lesson.subtitle}</h2>
-							</div>
-              <CardMedia
-                    component='img'
-                    height='160'
-                    image='https://source.unsplash.com/random/featured/?celebration'
-                />
-							<div className="end-lesson-page-message">
-                <h3>Good Job!</h3>
-                <p>You've finished the Lesson!</p>
-							</div>
+	return (
+		<>
+			<div>
+				<Container maxWidth="md" className="mb-6rem">
+					<div className="end-lesson-page-container">
+						<div className="end-lesson-page-title">
+							<h1>Lesson {lessonid}</h1>
+							<div className="vertical-line"></div>
+							<h2>{lesson.subtitle}</h2>
 						</div>
-					</Container>
-				</div>
+						<CardMedia
+							component="img"
+							height="160"
+							image="https://source.unsplash.com/random/featured/?celebration"
+						/>
+						<div className="end-lesson-page-message">
+							<h3>Good Job!</h3>
+							<p>You've finished the Lesson!</p>
+						</div>
+					</div>
+				</Container>
+			</div>
 
-      <EndBottomControls/>
-    </>
-  );
+			<EndBottomControls />
+		</>
+	);
 };
