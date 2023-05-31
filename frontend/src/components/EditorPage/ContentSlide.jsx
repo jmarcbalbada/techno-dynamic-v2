cbe789cf56acb7df31e940e2a75c2be72404f288
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-05-31 22:52:36 +0800

    Update Create Lesson

--- frontend/src/components/EditorPage/ContentSlide.jsx
+++ frontend/src/components/EditorPage/ContentSlide.jsx
@@ -1,40 +1,67 @@
 import React from 'react'
-import { Paper, TextField, Button } from '@mui/material'
-import './EditorPage.css'
+import { Button, Paper, TextField } from '@mui/material'
+import "./ContentSlide.css";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-export const ContentSlide = ( { slideNum, contentSlideHandler, slideContent }) => {
+export const ContentSlide = ( { index, deleteContent, ...props } ) => {
+
+    const deleteContentHandler = () => {
+        deleteContent(index);
+    }
+
+    const onTextChange = (e) => {
+        if (props.onTextChange) {
+            props.onTextChange(index, e.target.value);
+        }
+    }
+
   return (
-    <Paper elevation={16} sx={{
-        padding: '2rem',
-        marginBottom: '2rem',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        }}>
-
-        <div className="lesson-editor-content">
-            <span>Content:</span>
-        </div>
-
-        <TextField
-            onChange={(e) => contentSlideHandler(e, slideNum - 1)}
-            value={slideContent}
-            size="large"
-            placeholder="Write Something..."
-            multiline
-            rows={12}
+    <Paper elevation={16} className="content-title-paper" sx={{
+        position: "relative",
+        backgroundColor: "#f5f5f5",
+        marginTop: "2rem",
+        borderRadius: "10px",
+      }}>
+        <Button
+            onClick={() => {deleteContentHandler()}}
+            variant='text'
+            color='error'
             sx={{
-                width: '70%',
+                position: "absolute",
+                top: "2rem",
+                right: "2rem",
             }}>
-        </TextField>               
-
-        <div className="lesson-editor-buttons">
-            <Button variant="contained">Add Url</Button>
-            <Button variant="contained">Add Files</Button>
+                <DeleteForeverIcon />                
+        </Button>
+        <div className="content-page-title-input">
+          <span>Content:</span>
+          <TextField 
+            onChange={onTextChange}
+            required
+            size="small"
+            margin="dense"
+            multiline
+            minRows={10}
+            maxRows={15}
+            sx={{
+              width: "70%",
+              marginBottom: "1rem",
+            }}>
+          </TextField>
+          <div className='content-attach-buttons'>
+            <Button variant="contained" sx={{
+                backgroundColor: "#3F3F3F",
+            }}>
+                Add Url
+            </Button>
+            <Button variant="contained" sx={{
+                backgroundColor: "#3F3F3F",
+            }}>
+                Add Files
+            </Button>
+          </div>
+          <span className='content-slide-counter'>Page {index + 1}</span>
         </div>
-
-        <div className="lesson-editor-page-number">Slide {slideNum}</div>
-    </Paper>
+      </Paper>
   )
 }
