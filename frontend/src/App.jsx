d6de71c594c285ca1278a71650271555fd4e9d24
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-09-13 14:27:34 +0800

    Initialize Fresh Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,35 @@
-import { Routes, Route } from 'react-router';
-import { Dashboard } from './components/Dashboard/Dashboard';
-import { LessonPage } from './components/LessonPage/LessonPage';
-import { EditorPage } from './components/EditorPage/EditorPage';
-import { NotFound } from './components/NotFound/NotFound';
-import { EndLessonPage } from './components/EndLessonPage/EndLessonPage';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<Dashboard />}></Route>
+function App() {
+  const [count, setCount] = useState(0)
 
-				<Route
-					path="lessons/:lessonid/pages/:pageid"
-					element={<LessonPage />}
-				></Route>
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-				<Route path="lessons/:lessonid/end" element={<EndLessonPage />}></Route>
-
-				<Route path="editor" element={<EditorPage />}></Route>
-
-				<Route path="*" element={<NotFound />}></Route>
-			</Routes>
-		</>
-	);
-};
-
-export default App;
+export default App
