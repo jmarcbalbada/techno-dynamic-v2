1b6cbee3184b988dd93893b6936573b0a5e0940e
Louisse Amadeo Romero <louisseamadeo.romero@cit.edu>
2023-09-29 00:22:58 +0800

    Implement LessonCards

--- frontend/src/components/dashboard/LessonCards.jsx
+++ frontend/src/components/dashboard/LessonCards.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { useAuth } from 'hooks/useAuth';
 
-const LessonCards = () => {
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import EditIcon from '@mui/icons-material/Edit';
+
+const LessonCards = (props) => {
+  const { user } = useAuth();
+  const { title, description } = props;
+
   return (
-    <div>LessonCards</div>
-  )
-}
+    <Card
+      sx={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+      }}>
+      {/* TODO: add onClick handler to navigate to lesson page */}
+      <CardActionArea>
+        <CardMedia
+          component='img'
+          height='140'
+          // TODO: change hardcoded image to actual image
+          image='https://source.unsplash.com/random/featured/?working,office'
+        />
+        <CardContent>
+          {/* TODO: change to actual details needed for the card */}
+          <Typography gutterBottom variant='h5' component='div'>
+            {title}
+          </Typography>
+          <Typography variant='body2' color='text.secondary'>
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      {/* TODO: change hardcoded teacher string to user.role */}
+      {user?.username === 'teacher' && (
+        // TODO: add onClick handler to navigate to edit lesson page
+        <CardActions>
+          <Button startIcon={<EditIcon />} fullWidth>
+            Edit
+          </Button>
+        </CardActions>
+      )}
+    </Card>
+  );
+};
 
-export default LessonCards
+export default LessonCards;
