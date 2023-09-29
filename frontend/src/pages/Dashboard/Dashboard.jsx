e69f9ff2c6fbd0d4f316b432121f4f1349cef2bc
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-09-29 13:13:13 +0800

    Update Lesson Cards

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -14,71 +14,87 @@ const lessons = [
   {
     title: 'Introduction',
     description: 'This is the first lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?working,office'
   },
   {
     title: 'Starting a New Venture',
     description: 'This is the second lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?working'
   },
   {
     title: 'Team Formation',
     description: 'This is the third lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?office'
   },
   {
     title: 'Market Segmentation',
     description: 'This is the fourth lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?working,office'
   },
   {
     title: 'TBU: Problem Discovery',
     description: 'This is the fifth lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?working'
   },
   {
     title: 'TBU: Primary Market Research',
     description: 'This is the sixth lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?office'
   },
   {
     title: 'TBU: Beachhead Market',
     description: 'This is the seventh lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?working,office'
   },
   {
     title: 'TBU: Product Specification and Full Life Cycle Use Case',
     description: 'This is the eighth lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?working'
   },
   {
     title: 'TBU: Value Proposition and Competitive Advantage',
     description: 'This is the ninth lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?office'
   },
   {
     title: 'WildChase!',
     description: 'This is the tenth lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?working,office'
   },
   {
     title: 'TBU: Business Model Canvas (BMC)',
     description: 'This is the eleventh lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?working'
   },
   {
     title: 'TBU: Making Customers',
     description: 'This is the twelfth lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?office'
   },
   {
     title: 'TBU: Feasibility Check',
     description: 'This is the thirteenth lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?working,office'
   },
   {
     title: 'TBU: Money Matters',
     description: 'This is the fourteenth lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?working'
   },
   {
     title: 'TBU: Business Model Canvas (BMC) Recap',
     description: 'This is the fifteenth lesson in the course.',
+    image: 'https://source.unsplash.com/random/featured/?office'
   },
   {
     title: 'WildRoar!',
     description: 'This is the sixteenth lesson in the course.',
-  },
+    image: 'https://source.unsplash.com/random/featured/?working,office'
+  }
 ];
 
 const Dashboard = () => {
-  const {user} = useAuth()
+  const { user } = useAuth();
   return (
     <Container>
       <h1>Dashboard</h1>
@@ -104,7 +120,11 @@ const Dashboard = () => {
           {/* TODO: change hardcoded lessoncards to actual lessons */}
           {lessons.map((lesson, index) => (
             <Grid item xs={12} md={6} lg={4} key={index}>
-              <LessonCards title={lesson.title} description={lesson.description} />
+              <LessonCards
+                title={lesson.title}
+                description={lesson.description}
+                image={lesson.image}
+              />
             </Grid>
           ))}
         </Grid>
