4541b83ed3c5c39b3fc41541f268b3e499599aa1
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-10-06 14:46:19 +0800

    Add RoleAccess

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,14 @@ import {
   createRoutesFromElements
 } from 'react-router-dom';
 import { AuthLayout } from 'hocs/AuthLayout';
+import { RoleAccess } from 'hocs/RoleAccess';
 import Login from 'pages/Login/Login';
 import Register from 'pages/Register/Register';
 import ProtectedLayout from 'hocs/ProtectedLayout';
 import Dashboard from 'pages/Dashboard/Dashboard';
+import Edit from 'pages/Edit/Edit';
+import Forbid from 'pages/Forbid/Forbid';
+import NotFound from 'pages/NotFound/NotFound';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,7 +21,12 @@ export const router = createBrowserRouter(
 
       <Route element={<ProtectedLayout />}>
         <Route index path='/' element={<Dashboard />} />
+        <Route element={<RoleAccess roles={['teacher']} />}>
+          <Route path='/edit' element={<Edit />} />
+        </Route>
       </Route>
+      <Route path='/forbid' element={<Forbid />} />
+      <Route path='*' element={<NotFound />} />
     </Route>
   )
 );
