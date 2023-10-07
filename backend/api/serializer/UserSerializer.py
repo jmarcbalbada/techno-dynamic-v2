48d7ba0b360347506c56d0f44b96f37e9a69994a
Carl Yebes <carlsebastian.yebes@cit.edu>
2023-10-07 14:09:20 +0800

    modify: Modify User's function and format

--- backend/api/serializer/UserSerializer.py
+++ backend/api/serializer/UserSerializer.py
@@ -5,3 +5,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'role']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'role': {'allow_blank': False}
+        }
+
+
+
