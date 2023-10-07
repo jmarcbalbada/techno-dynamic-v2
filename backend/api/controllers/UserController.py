f964ce8c0986203f573ebb3ad418014e0c1ed0ca
Carl Yebes <carlsebastian.yebes@cit.edu>
2023-10-07 14:09:20 +0800

    modify: Modify User's function and format

--- backend/api/controllers/UserController.py
+++ backend/api/controllers/UserController.py
@@ -6,7 +6,6 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from ..models import CustomUser
 
-from django.shortcuts import get_object_or_404
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -14,27 +13,38 @@ from rest_framework.permissions import IsAuthenticated
 class UserController():
     @api_view(['POST'])
     def login(request):
-        user = get_object_or_404(CustomUser, username=request.data['username'])
-        if not user.check_password(request.data['password']):
-            return Response({"message": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
+        username = request.data.get('username', None)
+        password = request.data.get('password', None)
+
+        if username is None or password is None:
+            return Response({"message": "Invalid username or password"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = CustomUser.objects.get(username=username)
+        except CustomUser.DoesNotExist:
+            return Response({"message": "Username does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
         token, created = Token.objects.get_or_create(user=user)
         serializer = UserSerializer(instance=user)
-        return Response({"token": token.key, "user": serializer.data})
+        return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
     @api_view(['POST'])
     def register(request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            user = CustomUser.objects.get(username=request.data['username'])
-            user.set_password(request.data['password'])
-            user.save()
-            token = Token.objects.create(user=user)
-            return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            serializer = UserSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                user = CustomUser.objects.get(username=request.data['username'])
+                user.set_password(request.data['password'])
+                user.save()
+                token = Token.objects.create(user=user)
+                return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
+            return Response({"message": "Invalid data provided"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"message": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @api_view(['GET'])
     @authentication_classes([SessionAuthentication, TokenAuthentication])
     @permission_classes([IsAuthenticated])
     def test_token(request):
-        return Response("passed for {}".format(request.user.email))
+        return Response("passed for {}".format(request.user.email), status=status.HTTP_200_OK)
