a68e98399e1438431c13ebb14f43d918b5d6ec11
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-10-17 20:18:10 +0800

    Update Min and Max

--- frontend/src/components/editor/BaseDetailsValidationSchema.jsx
+++ frontend/src/components/editor/BaseDetailsValidationSchema.jsx
@@ -3,11 +3,16 @@ import * as yup from 'yup';
 export const BaseDetailsValidationSchema = yup.object({
   lessonNumber: yup
     .number('Enter a number')
+    .min(1, 'Lesson number should be greater than 0')
     .required('Lesson number is required'),
   title: yup
     .string('Enter a title')
+    .min(2, 'Title should be of minimum 2 characters length')
+    .max(100, 'Title should be of maximum 100 characters length')
     .required('Title is required'),
   subtitle: yup
     .string('Enter a subtitle')
+    .min(2, 'Subtitle should be of minimum 2 characters length')
+    .max(300, 'Subtitle should be of maximum 300 characters length')
     .required('Subtitle is required')
 });
