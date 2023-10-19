bf3c4f89137bb1b034ccdd9d1a82cfc989d3d4ac
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-10-20 00:53:28 +0800

    Update Register

--- frontend/src/data/courseCategories.js
+++ frontend/src/data/courseCategories.js
@@ -1,10 +1,10 @@
 export const courseCategories = [
   {
-    value: 'BSCS',
+    value: 'BS Computer Science',
     label: 'BS Computer Science'
   },
   {
-    value: 'BSIT',
+    value: 'BS Information Technology',
     label: 'BS Information Technology'
   }
 ];
