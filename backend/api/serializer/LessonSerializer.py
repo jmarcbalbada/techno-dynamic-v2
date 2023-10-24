2ef9b41e3ae147a4856f8a2330c6ca881601bbdc
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-10-25 00:17:24 +0800

    Fix Serializer error

--- backend/api/serializer/LessonSerializer.py
+++ backend/api/serializer/LessonSerializer.py
@@ -1,14 +1,12 @@
 from rest_framework import serializers
 from api.model.Lesson import Lesson
-from api.serializer.ImageModelSerializer import ImageModelSerializer
 from api.serializer.LessonContentSerializer import LessonContentSerializer
 
 class LessonSerializer(serializers.ModelSerializer):
     pages = LessonContentSerializer(many=True, read_only=True)
     lesson_urls = LessonContentSerializer(many=True, read_only=True)
     lesson_files = LessonContentSerializer(many=True, read_only=True)
-    images = ImageModelSerializer(many=True, read_only=True)
 
     class Meta:
         model = Lesson
-        fields = ('id', 'lesson', 'contents', 'url', 'files', 'images')
+        fields = ['id', 'lessonNumber', 'title', 'subtitle', 'coverImage', 'pages', 'lesson_files', 'lesson_urls']
