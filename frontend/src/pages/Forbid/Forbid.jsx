34de5d1d0f21e4745c265f06e09b122b331c3a74
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-11-02 17:13:33 +0800

    Add Forbid Page

--- frontend/src/pages/Forbid/Forbid.jsx
+++ frontend/src/pages/Forbid/Forbid.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import { Box, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
+import styles from './Forbid.module.css';
 
 const Forbid = () => {
-  return (
-    <div>Forbid</div>
-  )
-}
+  const navigate = useNavigate();
+  const handleBacktoDashboard = () => {
+    navigate('/');
+  };
 
-export default Forbid
+  return (
+    <Container>
+      <Box mt={3} display='flex' flexDirection='column' alignItems='center'>
+        <img
+          src='/illustrations/Forbidden.png'
+          className={styles['forbid']}
+          alt='Done'
+        />
+        <Typography variant='h4' align='center' gutterBottom>
+          Forbidden
+        </Typography>
+        <Box>
+          <Button
+            onClick={handleBacktoDashboard}
+            size='large'
+            variant='contained'>
+            Dashboard
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Forbid;
