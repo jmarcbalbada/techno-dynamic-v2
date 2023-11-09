eb51d8bda282ca11fdf46842d1901c46253e42cb
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-11-09 21:10:11 +0800

    Backend QuickFix

--- backend/api/serializer/LessonSerializer.py
+++ backend/api/serializer/LessonSerializer.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from api.model.Lesson import Lesson
 from api.serializer.LessonContentSerializer import LessonContentSerializer
+from api.serializer.FileSerializer import FileSerializer
 
 class LessonSerializer(serializers.ModelSerializer):
     pages = LessonContentSerializer(many=True, read_only=True)
     lesson_urls = LessonContentSerializer(many=True, read_only=True)
-    lesson_files = LessonContentSerializer(many=True, read_only=True)
+    lesson_files = FileSerializer(many=True, read_only=True)
 
     class Meta:
         model = Lesson
