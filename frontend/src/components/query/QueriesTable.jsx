bfcea885d05a782e14b36008e5183f6087d7119e
Louisse Amadeo Romero <louisseamadeo.romero@cit.edu>
2023-11-10 05:31:40 +0800

    Make QueriesPage

--- frontend/src/components/query/QueriesTable.jsx
+++ frontend/src/components/query/QueriesTable.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { DataGrid } from '@mui/x-data-grid';
 
+import { QueriesService } from 'apis/QueriesService';
 import QueryDetailsDialog from './QueryDetailsDialog';
 
 import { Box } from '@mui/material';
@@ -14,121 +15,11 @@ import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'lessonNumber', headerName: 'Lesson', width: 130 },
-  { field: 'title', headerName: 'Lesson Title', width: 250 },
-  { field: 'lastName', headerName: 'Last Name', width: 180 },
-  { field: 'question', headerName: 'Questions', width: 300 }
-];
-
-const rows = [
-  {
-    id: 1,
-    lessonNumber: 1,
-    title: 'Introduction',
-    question: 'What is elevator pitch?',
-    lastName: 'Smith',
-    firstName: 'Waro',
-    course: 'BSCS',
-    year: 2,
-    response:
-      'An elevator pitch is a brief and compelling introduction that summarizes a product, service, or idea in a concise and engaging manner.'
-  },
-  {
-    id: 2,
-    lessonNumber: 2,
-    title: 'Starting a New Venture',
-    question: 'What is a venture?',
-    lastName: 'Johnson',
-    firstName: 'Justine',
-    course: 'BSIT',
-    year: 3,
-    response: 'A venture refers to a new business'
-  },
-  {
-    id: 3,
-    lessonNumber: 3,
-    title: 'Team Formation',
-    question: 'What are the members of a team?',
-    lastName: 'Williams',
-    firstName: 'Carl',
-    course: 'BSCS',
-    year: 1,
-    response:
-      'Team members are individuals with diverse skills and expertise who collaborate to achieve common goals and objectives.'
-  },
-  {
-    id: 4,
-    lessonNumber: 4,
-    title: 'Market Segmentation',
-    question: 'What is a market?',
-    lastName: 'Brown',
-    firstName: 'James',
-    course: 'BSCS',
-    year: 4,
-    response:
-      'A market is a specific group of potential customers or consumers who are interested in a particular product or service and are willing to make a purchase.'
-  },
-  {
-    id: 5,
-    lessonNumber: 5,
-    title: 'Lean Canvas',
-    question: 'What is lean canvas?',
-    lastName: 'Davis',
-    firstName: 'Noah',
-    course: 'BSIT',
-    year: 2,
-    response:
-      'Lean Canvas is a strategic management tool used by entrepreneurs and startups to develop a concise and visual one-page business plan.'
-  },
-  {
-    id: 6,
-    lessonNumber: 6,
-    title: 'Wild Chase!',
-    question: 'What is wild chase?',
-    lastName: 'Miller',
-    firstName: 'Ava',
-    course: 'BSCS',
-    year: 3,
-    response:
-      'Wild Chase refers to a high-energy and competitive pursuit or endeavor to achieve a challenging goal or objective.'
-  },
-  {
-    id: 7,
-    lessonNumber: 7,
-    title: 'Validation Preparation',
-    question: 'What are the preparations needed?',
-    lastName: 'Wilson',
-    firstName: 'Ethan',
-    course: 'BSIT',
-    year: 1,
-    response:
-      'Preparations include conducting market research, validating the product idea, analyzing competitors, and developing a marketing strategy.'
-  },
-  {
-    id: 8,
-    lessonNumber: 8,
-    title: 'Problem-Solution Validation',
-    question: 'What is a good problem?',
-    lastName: 'Moore',
-    firstName: 'Sophia',
-    course: 'BSIT',
-    year: 4,
-    response:
-      'A good problem is a significant and unsolved issue or challenge faced by a specific target audience, which presents a valuable opportunity for a business solution.'
-  },
-  {
-    id: 9,
-    lessonNumber: 9,
-    title: 'Market Size',
-    question: 'What is market size?',
-    lastName: 'Anderson',
-    firstName: 'Mason',
-    course: 'BSCS',
-    year: 2,
-    response:
-      'Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.Market size refers to the total number of potential customers or the total revenue that can be generated in a specific market segment during a defined period of time.'
-  }
+  { field: 'createdAt', headerName: 'Created At', width: 110 },
+  { field: 'lessonInfo', headerName: 'Lesson', width: 210 },
+  { field: 'fullName', headerName: 'Name', width: 210 },
+  { field: 'courseYear', headerName: 'Course & Year', width: 240 },
+  { field: 'preview', headerName: 'Preview', width: 320 }
 ];
 
 const QueriesTable = () => {
@@ -136,6 +27,7 @@ const QueriesTable = () => {
 
   const [selectedRow, setSelectedRow] = useState(null);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [queries, setQueries] = useState([]);
 
   const handleClose = useCallback(() => {
     navigate('/', { replace: true });
@@ -150,6 +42,48 @@ const QueriesTable = () => {
     setIsDialogOpen(false);
   };
 
+  useEffect(() => {
+    getQueries();
+  }, []);
+
+  const getQueries = async () => {
+  try {
+    const queryResponse = await QueriesService.list();
+    if (queryResponse) {
+      // Transform the response data into rows for the DataGrid
+      const formattedQueries = queryResponse.data.map((query) => ({
+        id: query.id,
+        lessonId: query.lesson.id,
+        lessonNumber: query.lesson.lessonNumber,
+        title: query.lesson.title,
+        firstName: query.user.first_name,
+        lastName: query.user.last_name,
+        preview: query.subqueries.question,
+        course: query.user.course,
+        year: query.user.year,
+        lessonInfo: `${query.lesson.lessonNumber} - ${query.lesson.title}`,
+        fullName: `${query.user.first_name} ${query.user.last_name}`,
+        courseYear:
+          query.user.course && query.user.year
+            ? `${query.user.year} - ${query.user.course}`
+            : 'Teacher',
+        preview: query.subqueries[0]
+          ? query.subqueries[0].question
+          : 'No question available',
+        createdAt: query.subqueries[0] ? query.subqueries[0].created_at : null
+      }));
+
+      formattedQueries.sort((a, b) => {
+        return b.createdAt - a.createdAt;
+      });
+
+      setQueries(formattedQueries);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
   return (
     <Container component='main'>
       <Box mt={4}>
@@ -162,7 +96,7 @@ const QueriesTable = () => {
           </Box>
           <Box mt={4}>
             <DataGrid
-              rows={rows}
+              rows={queries}
               columns={columns}
               initialState={{
                 pagination: {
@@ -172,6 +106,12 @@ const QueriesTable = () => {
               pageSizeOptions={[5, 6, 7, 8, 9, 10]}
               onRowClick={handleRowClick}
               singleRowSelection
+              sortModel={[
+                {
+                  field: 'createdAt',
+                  sort: 'desc', 
+                }
+              ]}
             />
           </Box>
         </Stack>
