3d92e6d2290586a59fab1fe06cd560dd8ff78e4d
Carl Yebes <carlsebastian.yebes@cit.edu>
2023-12-19 02:21:14 +0800

    modify: modify into config

--- backend/backend_django/settings.py
+++ backend/backend_django/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'backend_django.wsgi.application'
 # }
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.parse(config("DATABASE_URL"))
 }
 
 
