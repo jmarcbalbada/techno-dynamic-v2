ba2affe1060bd65bab620481b78e1ca86d819967
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2023-12-21 18:46:20 +0800

    Fix URL

--- frontend/src/data/config.js
+++ frontend/src/data/config.js
@@ -1,6 +1,6 @@
 import { isLocal } from "../utils/destinations";
 
-let apiUrl = 'https://techno-learn.onrender.com/'
+let apiUrl = 'https://techno-learn.onrender.com'
 
 if (isLocal) {
   apiUrl = 'http://127.0.0.1:8000/'
