4da1dbd33db5ebf6181fa0d90287df9015715f6e
John Marc Balbada <jmarcbalbada@gmail.com>
2024-08-03 23:57:54 +0800

    <refactor>[JM] - added some changes

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -3,22 +3,23 @@ import { useAuth } from '../../hooks/useAuth';
 import { Box, Container } from '@mui/material';
 import BasicInformation from '../../components/profile/BasicInformation';
 import UserInformation from '../../components/profile/UserInfo';
+import useTitle from 'hooks/useTitle';
 
 const Profile = () => {
   const { user } = useAuth();
+  useTitle('Profile');
 
   return (
     <Container>
       <Box>
         <h1>{user?.first_name}'s Profile</h1>
-        <Box width={1} display="flex" flexDirection="column" gap={1}>
+        <Box width={1} display='flex' flexDirection='column' gap={1}>
           <Box>
-          <BasicInformation 
-            user={user} 
-            //display the information below the papers
-            display={
-            <UserInformation 
-            />}/>
+            <BasicInformation
+              user={user}
+              //display the information below the papers
+              display={<UserInformation />}
+            />
           </Box>
         </Box>
       </Box>
